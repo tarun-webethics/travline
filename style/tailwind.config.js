fb253df87ebd9d71f12b1d06aa6b602204e111fe
tarun.webethics <tarun.webethics@gmail.com>
2025-01-03 11:32:52 +0530

    first commit

---
+++ style/tailwind.config.js
@@ -0,0 +1,34 @@
+tailwind.config = {
+   theme: {
+      colors: {
+         'orange': '#263A29',
+         'blue': '#41644A',
+         'grey': '#1F2937',
+         'lightgrey': '#9CA3AF',
+         'white': '#ffffff'
+      },
+      fontFamily: {
+         'poppins': ['Poppins', 'sans-serif'],
+         'merriweather': ['Merriweather', '-serif'],
+      },
+      extend: {
+         spacing: {
+            '8xl': '96rem',
+            '9xl': '128rem',
+         },
+         borderRadius: {
+            '4xl': '2rem',
+         },
+         fontFamily: {
+            'poppins': ['Poppins', 'sans-serif'],
+         },
+      },
+      container: {
+         padding: {
+            DEFAULT: '1rem',
+            lg: '4rem',
+         },
+         center: true,
+      },
+   },
+}
